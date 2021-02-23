b3a9ca883de8de620cc26c5d2e0c70b5b21774b5
Gabriel Borges <gbdesigns13@gmail.com>
2021-02-22 23:31:39 -0300

    Material-UI configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+import { Button } from '@material-ui/core';
+
 export default function Home() {
-  return <div>Clone youtube com Next</div>;
+  return (
+    <div>
+      Clone youtube com Next
+      <Button variant="outlined" color="secondary">
+        Primary
+      </Button>
+    </div>
+  );
 }
